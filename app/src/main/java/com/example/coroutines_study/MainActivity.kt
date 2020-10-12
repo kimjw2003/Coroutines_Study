f66bf0e2597c0e8f9ed36ada87eb1c8486389a2e
kimjw2003 <kimjw111003@gmail.com>
2020-10-12 17:19:53 +0900

    [UPDATE] GlobalScope

--- app/src/main/java/com/example/coroutines_study/MainActivity.kt
+++ app/src/main/java/com/example/coroutines_study/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.coroutines_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        test()
+    }
+
+    private fun test(){
+        GlobalScope.launch {
+            delay(1000L)
+            println("World!")
+        }
+
+        println("Hello,")
+        Thread.sleep(2000L)
     }
 }
